6bbe74c0bc8b11e72821fdff3db4d6de2b942b23
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-02 14:33:37 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesElements = document.querySelector('#boxes');
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+
+
