2bfcc12500562993e1a792612c20b3ca062588de
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-02 14:17:15 +0300

    done task-09.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+const formObj = {};
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    if (
+        !formElements.email.value ||
+        !formElements.password.value)
+    { alert("Заполните все поля"); }
+
+    formObj.email = formElements.email.value;
+    formObj.password = formElements.password.value;
+}
+console.log(formObj);
+    
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+buttonChangeColor.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = document.body.style.backgroundColor;
+
+})
