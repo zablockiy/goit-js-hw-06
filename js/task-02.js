6cc26d936abb60f8645b6ad2b4152ec568fc7fbd
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-02 10:13:50 +0300

    done task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingrMarkUp = ingredients.map((elem) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = elem;
+  itemEl.classList.add('item');
+  return itemEl;
+})
+const listEl = document.querySelector("#ingredients");
+listEl.append(...ingrMarkUp);
+
