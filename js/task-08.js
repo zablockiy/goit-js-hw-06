447f3ed16ebdeb9d94b7eca9cbe0867efde954cf
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-10-03 11:10:14 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,16 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 const formObj = {};
-form.addEventListener('submit', onFormSubmit);
+form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
-    event.preventDefault();
-    const formElements = event.currentTarget.elements;
-    if (
-        !formElements.email.value ||
-        !formElements.password.value)
-    { alert("Заполните все поля"); }
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  if (!formElements.email.value || !formElements.password.value) {
+    alert("Заполните все поля");
+  }
 
-    formObj.email = formElements.email.value;
-    formObj.password = formElements.password.value;
+  formObj.email = formElements.email.value;
+  formObj.password = formElements.password.value;
+    console.log(formObj);
+    event.currentTarget.reset();
 }
-console.log(formObj);
-    
-
-
